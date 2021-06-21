62c4770d74d0aafdbdf638179f705eb029270dfc
Arsyel <arsyel@agate.id>
2021-06-22 06:19:05 +0700

    register hatch asset scene relate

--- src/js/assetLibrary/animations.js
+++ src/js/assetLibrary/animations.js
@@ -1,5 +1,6 @@
 import { AssetType } from "../const/assetType";
 import { Assets } from "./assetGameplay";
+import { Assets as AssetsHatch } from "./assetHatch";
 
 export const Animations = {
 
@@ -28,6 +29,24 @@ export const Animations = {
 		start: 0,
 		end: 7,
 		frameSpeed: 12,
+		loop: false
+	},
+	egg_crack: {
+		key: "egg_crack",
+		type: AssetType.ANIMATION,
+		spritesheetRef: AssetsHatch.egg_crack.key,
+		start: 0,
+		end: 89,
+		frameSpeed: 24,
+		loop: false
+	},
+	egg_hatch: {
+		key: "egg_hatch",
+		type: AssetType.ANIMATION,
+		spritesheetRef: AssetsHatch.egg_hatch.key,
+		start: 0,
+		end: 29,
+		frameSpeed: 24,
 		loop: false
 	},
 
