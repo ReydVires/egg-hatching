8d3b8ffc6eb53d79c25dfc1cc6f2f20f0c1f30d8
Arsyel <arsyel@agate.id>
2021-06-22 09:32:00 +0700

    add audio asset and register

---
+++ src/js/assetLibrary/assetAudio.js
@@ -0,0 +1,36 @@
+import { AssetType } from "../const/assetType";
+
+export const Audios = {
+
+	sfx_click: {
+		key: "sfx_click",
+		url: ["/audio/sfx/sfx_click.mp3"],
+		type: AssetType.AUDIO
+	},
+	sfx_zap: {
+		key: "sfx_zap",
+		url: ["/audio/sfx/sfx_zap.mp3"],
+		type: AssetType.AUDIO
+	},
+	sfx_fill_up: {
+		key: "sfx_fill_up",
+		url: ["/audio/sfx/sfx_fill_up.mp3"],
+		type: AssetType.AUDIO
+	},
+	sfx_popup: {
+		key: "sfx_popup",
+		url: ["/audio/sfx/sfx_popup.mp3"],
+		type: AssetType.AUDIO
+	},
+	sfx_achievement: {
+		key: "sfx_achievement",
+		url: ["/audio/sfx/sfx_achievement.mp3"],
+		type: AssetType.AUDIO
+	},
+	sfx_beep: {
+		key: "sfx_beep",
+		url: ["/audio/sfx/sfx_beep.mp3"],
+		type: AssetType.AUDIO
+	},
+
+};
