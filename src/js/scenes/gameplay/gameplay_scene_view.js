b5f1dd6eb3290dacd1153f34ddc86c3de67baf3e
Arsyel <arsyel@agate.id>
2021-06-23 06:07:09 +0700

    implement refactor module

--- src/js/scenes/gameplay/gameplay_scene_view.js
+++ src/js/scenes/gameplay/gameplay_scene_view.js
@@ -1,13 +1,14 @@
-import { Animations } from "../../assetLibrary/animations";
-import { Assets } from "../../assetLibrary/assetGameplay";
 import { Audios } from "../../assetLibrary/assetAudio";
+import { BackgroundView } from "./background/background_view";
 import { CONFIG } from "../../const/gameInfo";
+import { CharView } from "./char/char_view";
+import { ElectricityView } from "./effect/electricity_view";
 import { FontAsset } from "../../assetLibrary/assetFont";
-import { Image } from "../../modules/gameobjects/image";
+import { HUDUIView } from "./ui/hud_view";
+import { OverlayFlashUIView } from "./ui/overlay_flash_view";
+import { PlayButtonUIView } from "./ui/play_button_view";
 import { ScreenUtility } from "../../helper/screenUtility";
-import { Sprite } from "../../modules/gameobjects/sprite";
 import { Text } from "../../modules/gameobjects/text";
-import { addAnimation } from "../../helper/animationHelper";
 
 export class GameplaySceneView {
 
@@ -21,7 +22,6 @@ export class GameplaySceneView {
 	/** @readonly */
 	evenNames = {
 		FILL_PROGRESS_BAR: "FILL_PROGRESS_BAR",
-		SHOW_FLASHLIGHT: "SHOW_FLASHLIGHT",
 		GOTO_HATCH: "GOTO_HATCH",
 		LOCK_INPUT: "LOCK_INPUT",
 		TAP_POINT_BUTTON: "TAP_POINT_BUTTON",
@@ -37,218 +37,36 @@ export class GameplaySceneView {
 	}
 
 	create () {
-		const {
-			centerX,
-			centerY,
-			width: screenWidth,
-			height: screenHeight
-		} = this._screenUtil;
+		const { height: screenHeight } = this._screenUtil;
 
-		const background = new Image(this._scene, centerX, centerY, Assets.background.key);
-		background.transform.setDisplayWidthAsScreenWidth(true);
+		const backgroundView = new BackgroundView(this._scene, this._screenUtil);
+		const charView = new CharView(this._scene, this._screenUtil, backgroundView.baseRatio);
+		const overlayUIView = new OverlayFlashUIView(this._scene, this._screenUtil);
+		const hudView = new HUDUIView(this._scene, this._screenUtil);
+		const playBtnView = new PlayButtonUIView(this._scene, this._screenUtil, backgroundView.baseRatio);
+		const electricityEffectView = new ElectricityView(this._scene, playBtnView.getDisplayPositionFromCoordinate(0.7, 0), backgroundView.hatchNestRatio);
 
-		const baseRatio = background.transform.displayToOriginalHeightRatio;
-
-		//#region Environment
-		const board = new Image(this._scene, centerX * 0.5, centerY * 0.7, Assets.board.key);
-		board.transform.setToScaleDisplaySize(baseRatio * 0.7);
-
-		const windowOnWall = new Image(this._scene, centerX * 1.5, centerY * 0.7, Assets.window.key);
-		windowOnWall.transform.setToScaleDisplaySize(baseRatio * 0.8);
-
-		const lamp = new Image(this._scene, centerX * 1.5, centerY * 1.05, Assets.lamp.key);
-		lamp.transform.setToScaleDisplaySize(baseRatio * 1.4);
-
-		const nestPosition = lamp.transform.getDisplayPositionFromCoordinate(0.5, 1.15);
-		const hatchNest = new Image(this._scene, nestPosition.x, nestPosition.y, Assets.nest.key);
-		hatchNest.transform.setToScaleDisplaySize(baseRatio * 1.35);
-
-		const eggPosition = hatchNest.transform.getDisplayPositionFromCoordinate(0.5, 0.3);
-		const egg = new Image(this._scene, eggPosition.x, eggPosition.y, Assets.blue_egg.key);
-		egg.transform.setToScaleDisplaySize(baseRatio * 1.25);
-		egg.gameObject.setOrigin(0.5, 1);
-
-		const char = new Sprite(this._scene, centerX * 0.5, centerY * 1.15, Assets.char_idle.key, 0);
-		char.transform.setToScaleDisplaySize(baseRatio * 1.5);
-
-		const leafLeft = new Image(this._scene, 0, screenHeight, Assets.leaf.key);
-		leafLeft.transform.setToScaleDisplaySize(baseRatio * 1.25);
-		leafLeft.gameObject.setOrigin(0, 1);
-
-		const leafRight = new Image(this._scene, screenWidth, screenHeight, Assets.leaf.key);
-		leafRight.transform.setToScaleDisplaySize(baseRatio * 1.25);
-		leafRight.gameObject.setOrigin(1, 1).setFlipX(true);
-		//#endregion
-
-		addAnimation(this._scene, Animations.char_idle);
-		addAnimation(this._scene, Animations.char_excited);
-
-		char.gameObject.on("animationcomplete-" + Animations.char_excited.key, () => {
-			char.gameObject.play(Animations.char_excited.key);
-			this.event.emit(this.evenNames.SHOW_FLASHLIGHT);
+		charView.onAnimExcitedComplete(() => {
+			overlayUIView.showFlashlight(() => this.event.emit(this.evenNames.GOTO_HATCH));
 		});
 
-		char.gameObject.play(Animations.char_idle.key);
-
-		const payPoint = 100;
-		
-		const playBtn = new Image(this._scene, centerX, screenHeight * 0.9, Assets.btn.key);
-		playBtn.transform.setToScaleDisplaySize(baseRatio * 0.5);
-
-		const playBtnLabelPosition = playBtn.transform.getDisplayPositionFromCoordinate(0.5, 0.5);
-		const playBtnLabel = new Text(this._scene, playBtnLabelPosition.x, playBtnLabelPosition.y, String(payPoint), {
-			align: "center",
-			fontFamily: FontAsset.cabin.key,
-			fontStyle: "bold",
-			fontSize: `${72 * playBtn.transform.displayToOriginalHeightRatio}px`,
-		});
-		playBtnLabel.gameObject.setOrigin(0.5);
-
-		const electricityBigPos = playBtn.transform.getDisplayPositionFromCoordinate(0.7, 0);
-		const electricityBig = new Sprite(this._scene, electricityBigPos.x, electricityBigPos.y, Assets.electricity.key, 0);
-		electricityBig.gameObject.setOrigin(0.5, 1).setVisible(false);
-		electricityBig.transform.setToScaleDisplaySize(hatchNest.transform.displayToOriginalHeightRatio * 0.45);
-
-		addAnimation(this._scene, Animations.electricity);
-
-		electricityBig.gameObject.on("animationcomplete-" + Animations.electricity.key, () => {
-			electricityBig.gameObject.setVisible(false);
-			this.event.emit(this.evenNames.FILL_PROGRESS_BAR, Audios.sfx_fill_up.key);
-		});
-
-		const hud = new Image(this._scene, centerX, 0, Assets.hud.key);
-		hud.transform.setDisplayWidthAsScreenWidth(true);
-		hud.gameObject.setOrigin(0.5, 0);
-
-		const eggProgressBarPos = hud.transform.getDisplayPositionFromCoordinate(0.173, 0.61);
-		const eggProgressBar = new Image(this._scene, eggProgressBarPos.x, eggProgressBarPos.y, Assets.progress_bar.key);
-		eggProgressBar.transform.setToScaleDisplaySize(hud.transform.displayToOriginalHeightRatio);
-		eggProgressBar.gameObject.setOrigin(0, 0.5);
-
-		const progressBar = this._scene.add.graphics().setVisible(false);
-		eggProgressBar.gameObject.setMask(progressBar.createGeometryMask());
-
-		this.event.on(this.evenNames.FILL_PROGRESS_BAR, () => {
-			this._scene.tweens.addCounter({
-				from: 0,
-				to: 1,
-				duration: 600,
-				ease: Phaser.Math.Easing.Cubic.InOut,
-				onUpdate: (tween) => {
-					const value = tween.getValue();
-					progressBar.clear();
-					progressBar.fillStyle(0xffffff, 1);
-					progressBar.fillRect(
-						eggProgressBar.gameObject.getTopLeft().x,
-						eggProgressBar.gameObject.getTopLeft().y,
-						value * eggProgressBar.gameObject.displayWidth,
-						eggProgressBar.gameObject.displayHeight
-					);
-				},
-				onComplete: () => {
-					char.gameObject.play(Animations.char_excited.key);
-				}
-			});
-		});
-
-		//#region Play button effect
-		const playBtnOriginScale = playBtn.gameObject.scale;
-		const playBtnTween = this._scene.tweens.create({
-			targets: [playBtn.gameObject],
-			loop: -1,
-			props: {
-				scale: {
-					getStart: () => playBtnOriginScale,
-					getEnd: () => playBtnOriginScale * 1.02
-				}
-			},
-			yoyo: true,
-			ease: Phaser.Math.Easing.Cubic.InOut,
-			duration: 300,
-		});
-		playBtnTween.play();
-
-		const targetToEffect = playBtn.gameObject;
-		const glowDimGrapics = this._scene.add.graphics();
-		glowDimGrapics.clear();
-		glowDimGrapics.fillStyle(0xfafafa, 1);
-		glowDimGrapics.fillRoundedRect(
-			targetToEffect.x - (targetToEffect.displayWidth / 2),
-			targetToEffect.y - (targetToEffect.displayHeight / 2),
-			targetToEffect.displayWidth,
-			targetToEffect.displayHeight,
-			30 * baseRatio
-		);
-
-		const idleGlowDimTween = this._scene.tweens.create({
-			targets: [glowDimGrapics],
-			props: {
-				alpha: { getStart: () => 0, getEnd: () => 0.25 }
-			},
-			yoyo: true,
-			loop: -1,
-			duration: 600,
-			ease: Phaser.Math.Easing.Cubic.InOut,
-		});
-		idleGlowDimTween.play();
-
-		const tapEffectTween = this._scene.tweens.create({
-			targets: [playBtn.gameObject],
-			props: {
-				scale: {
-					getStart: () => playBtnOriginScale,
-					getEnd: () => playBtnOriginScale * 0.95
-				}
-			},
-			yoyo: true,
-			duration: 120,
-			onComplete: () => {
-				this._scene.tweens.addCounter({
-					from: payPoint,
-					to: 0,
-					duration: 300,
-					onUpdate: (tween) => {
-						const value = Math.floor(tween.getValue());
-						playBtnLabel.gameObject.setText(String(value));
-					},
-					onComplete: () => {
-						playBtn.gameObject.setTexture(Assets.btn_gray.key);
-					},
-				});
-			},
-		});
-
-		playBtn.gameObject.setInteractive({ useHandCursor: true }).on(Phaser.Input.Events.POINTER_UP, () => {
+		playBtnView.onClick(() => {
 			this.event.emit(this.evenNames.TAP_POINT_BUTTON, Audios.sfx_zap.key);
 			this.event.emit(this.evenNames.LOCK_INPUT);
-			tapEffectTween.play();
-
-			electricityBig.gameObject.setVisible(true).play(Animations.electricity.key);
-			glowDimGrapics.clear();
-			idleGlowDimTween.stop();
-			playBtnTween.stop();
+			electricityEffectView.playAnim();
 		});
-		//#endregion
 
-		// Create Flashlight
-		const flashObject = this._scene.add.graphics().setAlpha(0).setVisible(false);
-		flashObject.fillStyle(0xfafafa, 1);
-		flashObject.fillRect(0, 0, screenWidth, screenHeight);
-
-		this.event.once(this.evenNames.SHOW_FLASHLIGHT, () => {
-			flashObject.setVisible(true);
-			this._scene.tweens.add({
-				targets: flashObject,
-				duration: 500,
-				alpha: 1,
-				onComplete: () => this.event.emit(this.evenNames.GOTO_HATCH),
+		electricityEffectView.onAnimElectricityComplete(() => {
+			this.event.emit(this.evenNames.FILL_PROGRESS_BAR, Audios.sfx_fill_up.key);
+			hudView.fillProgressBar(() => {
+				charView.playAnimExcited();
 			});
 		});
 
 		if (CONFIG.MODE === "PRODUCTION") return;
 		const versionDebug = new Text(this._scene, 0, screenHeight, CONFIG.VERSION, {
 			fontFamily: FontAsset.arial.key,
-			fontSize: `${18 * baseRatio}px`
+			fontSize: `${18 * backgroundView.baseRatio}px`
 		});
 		versionDebug.gameObject.setOrigin(0, 1);
 	}
